9dda32150d218b36dc858c2b8e364c5e870c78b5
Kenneth Ward <43589922+krward2@users.noreply.github.com>
2023-09-26 20:47:33 +0000

    Completed Exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeFromArray) {
+    removeFromArray.forEach((element) => {
+        array = array.filter((e) => {
+            return e !== element;
+        })
+    });
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
