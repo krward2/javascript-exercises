ae85578e2e52cddbff45d3d1edb182044ef8d49c
Kenneth Ward <43589922+krward2@users.noreply.github.com>
2023-09-26 19:34:41 +0000

    Complete Excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    if(num === 0) return "";
+    return string + repeatString(string, num - 1);
 };
 
 // Do not edit below this line
