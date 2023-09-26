58bafd4851955726cd1c30642b2d899dbeb10734
Kenneth Ward <43589922+krward2@users.noreply.github.com>
2023-09-26 21:32:58 +0000

    Exercise 5 passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (typeof (num1 + num2) !== "number" || num1 * num2 < 0) return "ERROR";
+    let start;
+    let end;
+    let sum = 0;
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    } else if (num2 > num1) {
+        start = num1;
+        end = num2;
+    } else {
+        return num1;
+    }
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
