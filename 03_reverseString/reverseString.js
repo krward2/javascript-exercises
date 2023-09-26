e62119cdb30af77851d356ccc3cb1e2280f0789f
Kenneth Ward <43589922+krward2@users.noreply.github.com>
2023-09-26 19:41:17 +0000

    Completed Excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if(string.length === 0) return '';
+    return string[string.length-1] + reverseString(string.substring(0, string.length-1))
 };
 
 // Do not edit below this line
