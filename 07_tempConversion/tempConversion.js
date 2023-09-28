2729d0d04700773f2d0ef3f002c50ad4b9f76e8f
Kenneth Ward <43589922+krward2@users.noreply.github.com>
2023-09-28 20:47:18 +0000

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const CONVERSION_FACTOR = 5 / 9;
+  return ((temp - 32) * CONVERSION_FACTOR).toFixed(2);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const CONVERSION_FACTOR = 9 / 5;
+  return (temp * CONVERSION_FACTOR + 32).toFixed(2);
 };
 
 // Do not edit below this line
